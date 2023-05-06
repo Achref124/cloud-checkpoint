d702f2006ae317aa843de5f62495a2a2a3532e2a
achref124 <ouerfelliiachref@gmail.com>
2023-05-06 12:42:36 +0100

    first add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './Components/Map';
+import Navb from './Components/Navbar';
 
 function App() {
+  const location = {
+    address: 'R5V9+FR Ariana',
+    lat: 36.8437076,
+    lng: 10.1690117,
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage:"url(https://static.vecteezy.com/system/resources/previews/001/984/880/original/abstract-colorful-geometric-overlapping-background-and-texture-free-vector.jpg)",backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
+      <Navb />
+      <Map location={location} zoomLevel={17}/>
     </div>
   );
 }
